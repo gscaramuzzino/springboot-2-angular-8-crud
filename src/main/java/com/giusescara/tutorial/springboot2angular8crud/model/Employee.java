4e7277ddb25e1b81f0af19aa044b4deab17ef252
Giuseppe Scaramuzzino <giuse.scara@gmail.com>
2019-11-26 21:59:22 +0100

    * Fix first_name column in Employee * Change path to create test db * Add unit test to cover all rest endpoints inside EmployeeController

--- src/main/java/com/giusescara/tutorial/springboot2angular8crud/model/Employee.java
+++ src/main/java/com/giusescara/tutorial/springboot2angular8crud/model/Employee.java
@@ -10,7 +10,7 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @Column(name = "firs_name")
+    @Column(name = "first_name")
     private String firstName;
 
     @Column(name = "last_name")
